c92f5410e430b292affe7659f5eb7edacfeeb855
David Goldfarb <deg@degel.com>
2013-07-08 19:17:30 +0300

    Initial 'lein init'

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to redmapel
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/redmapel/core_test.clj
@@ -0,0 +1,7 @@
+(ns redmapel.core-test
+  (:require [clojure.test :refer :all]
+            [redmapel.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
