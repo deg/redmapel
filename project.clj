20ff820a5c99978384fac05a918e15e5df7725f8
David Goldfarb <deg@degel.com>
2013-07-09 14:10:45 +0300

    Use Degel utils lib.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+
+                 ;; Degel's Clojure utility library
+                 [degel-clojure-utils "0.1.3"]])
