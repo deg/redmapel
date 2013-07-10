b68ad11c1d21cba1f9f0dbb6dc7ea0fcdd9f4f14
David Goldfarb <deg@degel.com>
2013-07-10 15:01:46 +0300

    Add copyright/contact/license block.

--- src/redmapel/node.clj
+++ src/redmapel/node.clj
@@ -6,6 +6,7 @@
 ;;; be found in the file epl-v10.html at the root of this distribution.
 ;;; By using this software in any fashion, you are agreeing to be bound by the
 ;;; terms of this license.
+;;;
 ;;; You must not remove this notice, or any other, from this software.
 
 (ns redmapel.node
