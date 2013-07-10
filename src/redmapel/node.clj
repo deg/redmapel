c0a3cac05ceace316418fe00869ded5ff9c9071f
David Goldfarb <deg@degel.com>
2013-07-10 11:01:36 +0300

    First implementation of watchers.

--- src/redmapel/node.clj
+++ src/redmapel/node.clj
@@ -27,13 +27,52 @@
 (defn- path-path [path]
   (-> path node-path (conj :path)))
 
-
-(defn node-assoc [node path value]
-  (assoc-in
-   (assoc-in node
-             (path-path path) path)
-   (value-path path) value))
+(defn- watchers-path [path]
+  (-> path node-path (conj :watchers)))
 
 
 (defn node-get [node path]
   (get-in node (value-path path)))
+
+(defn- heads [l]
+  (reductions conj [] l))
+
+(defn- partition-pairs [l]
+  (into {}
+        (map #(vector (ffirst %) (map second %))
+             (partition-by first l))))
+
+(defn watchers [node path]
+  (partition-pairs
+   (mapcat #(get-in node (watchers-path %))
+           (heads path))))
+
+
+(defn node-assoc [node path value]
+  (let [old-value (node-get node path)]
+    (if (= old-value value)
+      node
+      (let [{:keys [before after]} (watchers node path)]
+        (if (every? #(% node path old-value value) before)
+          (let [new-node (assoc-in
+                          (assoc-in node (path-path path) path)
+                          (value-path path) value)]
+            (doseq [f after]
+              (f new-node path old-value value))
+            new-node)
+          node)))))
+
+
+(defn toy-watch [node path old new]
+  (dev/dbg "Watch" (str "Changed " path " from " old " to " new)))
+
+(defn toy-pred [node path old new]
+  (even? new))
+
+
+(defn node-watch
+  "Watch types:
+   :before - can return false to abort the operation
+   :after - just for side-effects"
+  [node path watch-type watch-fn]
+  (update-in node (watchers-path path) conj [watch-type watch-fn]))
