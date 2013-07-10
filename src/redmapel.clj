00323c7b7645074c55fb126b98493d2b6e30ad86
David Goldfarb <deg@degel.com>
2013-07-10 22:26:54 +0300

    Wrapper functions to hold state tree in an atom.

--- src/redmapel.clj
+++ src/redmapel.clj
@@ -9,9 +9,78 @@
 ;;;
 ;;; You must not remove this notice, or any other, from this software.
 
-(ns redmapel)
+(ns redmapel
+  (:require [redmapel.node :as rml-node]
+            [degel.cljutil.devutils :as dev]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defmacro defmapel
+  "Initialize a state tree."
+  ([rml-tree]
+     `(def ~rml-tree (atom (rml-node/make-node))))
+    ([doc rml-tree]
+     `(def ~rml-tree ~doc (atom (rml-node/make-node)))))
+
+
+(defn empty!
+  "Empty a state tree, discarding everything. Note that this global action will
+   not trigger any alerts."
+  [rml-tree]
+  (reset! rml-tree (rml-node/make-node)))
+
+
+(defn describe
+  "Print a verbose description of the state tree's contents"
+  [rml-tree]
+  (rml-node/describe-node @rml-tree))
+
+
+(defn fetch
+  "Fetch a value from the state tree. Path is a vector of identifiers.
+
+   Example: (fetch tree [:users :account-info :user-id])"
+  [rml-tree path]
+  (rml-node/node-get @rml-tree path))
+
+
+(defn put!
+  "Store a value into the state tree. Path is a vector of identifiers.
+
+  Example: (put! tree [:users :account-info :user-id] \"David\")"
+  [rml-tree path value]
+  (swap! rml-tree rml-node/node-assoc path value))
+
+
+(defn guard! [rml-tree path f]
+  "Set up a guard function.
+
+  The function f is called when anyone tries to modify the tree at path, before
+  the modification actually occurs . Note, that this watches the entire subtree
+  below path. That is, if the path argument is [:a :b], f will be called on
+  (put! tree [:a :b]) or (put! tree [:a :b :c :d]).
+
+  The arguments passed to the function are tree, path, old-value, and
+  new-value. Note that the path is of the actual value being changed, which
+  might be far below the path passed to guard!.
+
+  If f returns falsey, it will prevent the modification. If multiple guards are
+  in place, any one returning falsey is sufficient to prevent the
+  modification."
+  (swap! rml-tree rml-node/node-watch path :before f))
+
+
+(defn alert!
+  "Set up a trigger function.
+
+  The function f is called when anyone modifies the tree at path, immediately
+  after the modification actually occurs . Note, that this watches the entire
+  subtree below path. That is, if the path argument is [:a :b], f will be
+  called on (put! tree [:a :b]) or (put! tree [:a :b :c :d]).
+
+  The arguments passed to the function are tree, path, old-value, and
+  new-value. Note that the path is of the actual value being changed, which
+  might be far below the path passed to guard!.
+
+  The return value from f is ignored."
+  [rml-tree path f]
+  (swap! rml-tree rml-node/node-watch path :after f))
--- test/redmapel_test.clj
+++ test/redmapel_test.clj
@@ -13,6 +13,26 @@
   (:require [clojure.test :refer :all]
             [redmapel :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I do nothing."
-    (is (= 1 1))))
+
+(deftest put-fetch
+  (defmapel tree1)
+  (is (= (type tree1) clojure.lang.Atom))
+  (put! tree1 [:a] 1)
+  (put! tree1 [:a :b] 12)
+  (put! tree1 [:b] 2)
+  (put! tree1 [:b :a] 21)
+  (is (= (fetch tree1 [:a]) 1))
+  (is (= (fetch tree1 [:a :b]) 12))
+  (is (= (fetch tree1 [:b]) 2))
+  (is (= (fetch tree1 [:b :a]) 21))
+  (is (= (fetch tree1 [:c]) nil)))
+
+(deftest guard
+  (defmapel tree1)
+  (guard! tree1 [:a] (fn [_ _ _ new-value] (even? new-value)))
+  (put! tree1 [:a :a] 1)
+  (put! tree1 [:a :b] 2)
+  (is (= (fetch tree1 [:a]) nil))
+  (is (= (fetch tree1 [:a :a]) nil))
+  (is (= (fetch tree1 [:a :b]) 2))
+  (is (= (fetch tree1 [:a :c]) nil))))
