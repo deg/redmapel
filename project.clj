c2941412c5b28b8fa35392ac5da65df6ffb115cd
David Goldfarb <deg@degel.com>
2013-08-07 23:02:54 +0300

    Use degel-clojure-utils 0.1.6 so we can work in cljs. Bump our version to 0.1.6.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
 ;;; You must not remove this notice, or any other, from this software.
 
 
-(defproject redmapel "0.1.5"
+(defproject redmapel "0.1.6"
   :description "Redmapel manages trees of state."
   :url "https://github.com/deg/redmapel"
   :license {:name "Eclipse Public License"
@@ -24,7 +24,7 @@
                  [org.clojure/clojure "1.5.1"]
 
                  ;; Degel's Clojure utility library
-                 [degel-clojure-utils "0.1.5"]]
+                 [degel-clojure-utils "0.1.6"]]
 
   :profiles {:dev
              { :plugins [[lein-marginalia "0.7.1"]]}})
